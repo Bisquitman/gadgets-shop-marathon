e27bfbaee8818d84fab4dc6913de8a04ee4ff6be
Bisquitman <lynx8309@gmail.com>
2022-07-01 16:36:04 +0300

    Day 3

--- src/index.js
+++ src/index.js
@@ -3,4 +3,37 @@ import './card.html';
 import './cart.html';
 import './index.scss';
 
+// import Swiper JS
+import Swiper, { Thumbs, Scrollbar, Pagination } from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/pagination';
 
+// init Swipers:
+const thumbSwiper = new Swiper('.card__slider-thumb', {
+  spaceBetween: 44,
+  slidesPerView: 3,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  modules: [Scrollbar],
+});
+new Swiper('.card__image', {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  thumbs: {
+    swiper: thumbSwiper,
+    slideThumbActiveClass: 'card__thumb-btn_active',
+  },
+  modules: [Thumbs],
+});
+new Swiper('.recommended__carousel', {
+  spaceBetween: 30,
+  slidesPerView: 5,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  modules: [Pagination],
+});
